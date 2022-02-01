f7c3ab7bc6fa4d9c1ff5fab643d9b8df5b36c3d1
Daiva Rajackiene <daiva.rajackiene@gmail.com>
2022-02-01 14:02:11 +0200

    camel case bet neveikia

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
 console.log("app.js")
+const functions =require("./functions")
+
+const rez = functions.makeCamelCase("Ar veikia Camel Case tikrai?")
+
+console.log("rez===", rez);
---
+++ src/functions.js
@@ -0,0 +1,23 @@
+function makeCamelCase(str){
+console.log(str)
+
+const arrFromString = str.split(" ")
+console.log("arrFromString===", arrFromString);
+const arrayAfterProcess = [];
+arrFromString.forEach((element, index, arr)=>{
+    if(element!==" "){
+        arrayAfterProcess.push(element)
+    }else {
+arr[index+1] = arr[index +1].toUpperCase()
+    }
+})
+arrayAfterProcess[0]=arrayAfterProcess[0].toLowerCase()
+const stringFromArr = arrayAfterProcess.join("")
+console.log("stringFromArr===", stringFromArr)
+return stringFromArr
+}
+makeCamelCase("ar veikia kupranugario case?")
+
+module.exports = {
+    makeCamelCase,
+}
